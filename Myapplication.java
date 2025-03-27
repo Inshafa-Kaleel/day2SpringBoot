328447a0fb3434b92e45a170de61f7ff9733473a
Inshafa-Kaleel <154611829+Inshafa-Kaleel@users.noreply.github.com>
2025-03-27 23:32:44 +0530

    Add files via upload

---
+++ Myapplication.java
@@ -0,0 +1,23 @@
+package lk.ac.vau.fas.ict.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app")
+
+public class Myapplication {
+	@GetMapping("/msg")
+	public String myMessage() {
+		return "Hello SpringBoot";
+	}
+	@GetMapping("/name")
+	public String myName() {
+		return "My name is SpringBoot";
+	}
+	@GetMapping("/myProfile")
+	public String myProfile() {
+		return "My name is Manuka and my reg no is 2020ICT43";
+	}
+}
